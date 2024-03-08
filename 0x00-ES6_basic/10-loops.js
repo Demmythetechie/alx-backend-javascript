89b41a7e31fe178d4ed791d4745c5071412a96d2
Demmythetechie <demmythetechie@gmail.com>
2024-03-08 17:41:12 +0100

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
+  for (const idx of array) {
+    array[index] = `${appendString}${idx}`;
+    index += 1;
+  }
+
+  return array;
+}
