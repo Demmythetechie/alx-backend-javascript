be1ca911fc95c3c12b9cd558398779616d7c9292
Demmythetechie <demmythetechie@gmail.com>
2024-03-12 20:18:13 +0100

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,18 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  const pr = Promise.all([promise1, promise2]).then((values) => {
+
+  const prs = Promise.all([promise1, promise2]);
+  const pr = prs.then((values) => {
     console.log(values[0].body, values[1].firstName, values[1].lastName);
   });
 
-  const pr2 = Promise.all([promise1, promise2]).catch(() => {
+  const pr2 = prs.catch(() => {
     console.log('Signup system offline');
   });
-  return (pr, pr2);
+  return (prs);
 }
 
 export default handleProfileSignup;
