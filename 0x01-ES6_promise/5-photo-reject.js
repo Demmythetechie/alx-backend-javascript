4040c07bb4479ab29bf702f69595b2c067c7c639
Demmythetechie <demmythetechie@gmail.com>
2024-04-12 00:09:04 +0100

    5. Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,7 @@
 export default function uploadPhoto(filename) {
   const promise = new Promise((reject) => {
-    reject(`${filename} cannot be processed`);
+    const err = new Error(`${filename} cannot be processed`);
+    reject(err);
   });
   return promise;
 }
