cdf5627618bfe6894c980ac0211b69b302ea822d
Demmythetechie <demmythetechie@gmail.com>
2024-04-17 13:19:32 +0100

    7. Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+class Airport {
+  constructor (name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name () {
+    return this._name;
+  }
+
+  set name (nm) {
+    if (typeof nm !== 'string') {
+      throw new TypeError('Name must be a String');
+    } else {
+      this._name = nm;
+    }
+  }
+
+  get code () {
+    return this._code;
+  }
+
+  set code (cd) {
+    if (typeof cd !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = cd;
+    }
+  }
+
+  toString() {
+    return `[${typeof new Airport} ${this._code}]`;
+  }
+}
+
+export default Airport;
